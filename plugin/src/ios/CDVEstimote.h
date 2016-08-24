4708dcc4d3317621f762a81eb3a35bddcfe02afd
Rafael Diaz <rd@lanista-training.com>
2016-08-24 18:18:14 +0200

    Test implementation

--- plugin/src/ios/CDVEstimote.h
+++ plugin/src/ios/CDVEstimote.h
@@ -1,18 +1,22 @@
 #import <Cordova/CDV.h>
 
 #import "ESTBeaconManager.h"
+#import "ESTNearableManager.h"
 #import "ESTBeaconRegion.h"
 #import "ESTBeacon.h"
 
 @interface CDVEstimote : CDVPlugin<ESTBeaconManagerDelegate>
 
 @property (nonatomic, strong) ESTBeaconManager *beaconManager;
+@property (nonatomic, strong) ESTNearableManager *nearableManager;
 @property (nonatomic, strong) ESTBeaconRegion *region;
 
 @property (strong) NSString* callbackId;
 
 - (void)startRanging:(CDVInvokedUrlCommand*)command;
 - (void)stopRanging:(CDVInvokedUrlCommand*)command;
+
+- (void)startRangingForType:(CDVInvokedUrlCommand*)command;
 
 
 @end
